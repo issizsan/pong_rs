b52f0f53fbcc026ebb403a395278524ab5a836f5
ordaysan <cyneric18@protonmail.com>
2020-12-05 17:46:55 +0500

    Changed boilerplate, created MainState

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,86 @@
-use ggez::{graphics, Context, ContextBuilder, GameResult};
-use ggez::event::{self, EventHandler};
+use ggez;
+use ggez::event;
+use ggez::graphics;
+use ggez::graphics::{Color, DrawMode, DrawParam, Mesh};
+use ggez::nalgebra as na;
+use ggez::{Context, GameResult};
 
-fn main() {
-    // Make a Context.
-    let (mut ctx, mut event_loop) = ContextBuilder::new("my_game", "Cool Game Author")
-		.build()
-		.expect("aieee, could not create ggez context!");
+const SCREEN_WIDTH: u32 = 500;
+const SCREEN_HEIGHT: u32 = 500;
 
-    // Create an instance of your event handler.
-    // Usually, you should provide it with the Context object to
-    // use when setting your game up.
-    let mut my_game = MyGame::new(&mut ctx);
+const PADDLE_OFFSET: f32 = 20.0;
+const PADDLE_WIDTH: f32 = 10.0;
+const PADDLE_HEIGHT: f32 = 70.0;
 
-    // Run!
-    match event::run(&mut ctx, &mut event_loop, &mut my_game) {
-        Ok(_) => println!("Exited cleanly."),
-        Err(e) => println!("Error occured: {}", e)
+/* struct Paddle {
+    x: f32,
+    y: f32,
+    width: f32,
+    height: f32,
+} */
+
+struct MainState { 
+    paddle1: Mesh,
+    paddle2: Mesh,
+}
+
+impl MainState { 
+    fn new(ctx: &mut Context) -> GameResult<MainState> {
+        let (screen_w, screen_h) = graphics::drawable_size(ctx);
+        let (screen_w_half, screen_h_half) = (screen_w * 0.5, screen_h * 0.5);
+
+
+        let paddle1: Mesh = Mesh::new_rectangle (
+            ctx,
+            DrawMode::fill(),
+            graphics::Rect {
+                x: (PADDLE_WIDTH / 2.0) + PADDLE_OFFSET as f32,
+                y: screen_h_half,
+                w: PADDLE_WIDTH,
+                h: PADDLE_HEIGHT,
+            },
+            graphics::BLACK,
+        )?;
+
+        let paddle2: Mesh = Mesh::new_rectangle (
+            ctx,
+            DrawMode::fill(),
+            graphics::Rect {
+                x: (SCREEN_WIDTH as f32 - PADDLE_OFFSET as f32),
+                y: screen_h_half,
+                w: PADDLE_WIDTH,
+                h: PADDLE_HEIGHT,
+            },
+            graphics::BLACK,
+        )?;
+
+        let s = MainState {
+            paddle1,
+            paddle2,
+        };
+
+        Ok(s)
     }
 }
 
-struct MyGame {
-    // Your state here...
-}
 
-impl MyGame {
-    pub fn new(_ctx: &mut Context) -> MyGame {
-        // Load/create resources such as images here.
-        MyGame {
-            // ...
-        }
-    }
-}
-
-impl EventHandler for MyGame {
-    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
-        // Update code here...
+impl event::EventHandler for MainState {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult {
         Ok(())
     }
 
-    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
-        graphics::clear(ctx, graphics::WHITE);
-        // Draw code here...
-        graphics::present(ctx)
+    fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        graphics::clear(ctx, [0.1, 0.2, 0.3, 1.0].into());
+
+
+        Ok(())
     }
+
+}
+
+pub fn main() -> GameResult {
+    let cb = ggez::ContextBuilder::new("Pong", "ordaysan");
+    let (ctx, event_loop) = &mut cb.build()?;
+    let state = &mut MainState::new(ctx)?;
+    event::run(ctx, event_loop, state)
 }
